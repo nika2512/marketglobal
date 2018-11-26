7deb23ea9bf11a47f30b505cb9a85606d5ff5c5f
nika2512 <44405352+nika2512@users.noreply.github.com>
2018-11-26 18:15:43 +0700

    Create ResponseForge.java

---
+++ model/forge/ResponseForge.java
@@ -0,0 +1,14 @@
+package ru.const24.globalmarket.model.forge;
+
+import java.util.List;
+
+/**
+ * TODO: add doc!
+ */
+public class Forge1Response {
+    public final List<Forge1CurrencyInfo> currencyInfoList;
+
+    public Forge1Response(List<Forge1CurrencyInfo> currencyInfoList) {
+        this.currencyInfoList = currencyInfoList;
+    }
+}
